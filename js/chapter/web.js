c2e8f754e7d0a8d5708e097fcbeb0f97a3019716
Pandark <apache.kof@gmail.com>
2011-10-07 14:27:34 +0200

    traduction des fonctions du onzième chapitre

--- js/chapter/web.js
+++ js/chapter/web.js
@@ -1,3 +1,3 @@
 load("FunctionalTools.js");
 
-var timeWriter = "<html>\n  <head><title>The time</title></head>\n  <body>\n    <h1>The time</h1>\n    <p>The time is\n      <script type=\"text/javascript\">\n        var time = new Date();\n        document.write(time.getHours() + \":\" + time.getMinutes());\n      </script>\n    </p>\n  </body>\n</html>";
+var horlogeParlante = "<html>\n  <head><title>Le temps</title></head>\n  <body>\n    <h1>Le temps</h1>\n    <p>Il est\n      <script type=\"text/javascript\">\n        var time = new Date();\n        document.write(time.getHours() + \"h\" + time.getMinutes());\n      </script>\n    </p>\n  </body>\n</html>";
